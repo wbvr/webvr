d932f78b58e067d573e67fdf1948868ca5165396
lizhilei <lizhilei92@sina.com>
2016-08-29 14:37:28 +0800

    文字立体

--- static/js/vr_countdown.js
+++ static/js/vr_countdown.js
@@ -5,57 +5,89 @@ function countdown(callback) {
         console.warn('callback is not function');
     }
 
-    var curtainGeometry = new THREE.PlaneGeometry( curtain_width, curtain_height );
+
+
+    var pointLight = new THREE.PointLight( 0xffffff, 1.5 );
+    pointLight.position.set( 0, 100, 90 );
+    scene.add( pointLight );
+
+    var colorhash  = "00ffff";
+    hex = colorhash;
+    pointLight.color.setHex( parseInt( colorhash, 16 ) );
+
+
+    var curtainGeometry = new THREE.PlaneGeometry( 300, 200 );
     var curtain = new THREE.Mesh( curtainGeometry ,new THREE.MeshBasicMaterial({
         color: 0x000000
     }));
-    curtain.position.set(0,0,-399);
+    curtain.position.set(0,50,-399);
 
-    var text = new THREE.TextGeometry(count,{
-        size: 20,
-        height: 0,
-        weight: 'normal',
-        font:  font,
-        style: 'normal',
-        bevelThickness: 1,
-        bevelSize: 1,
-        bevelSegments: 1,
-        curveSegments: 50,
-        steps: 1
+    var textMaterial = new THREE.MultiMaterial( [
+        new THREE.MeshPhongMaterial( { color: 0xffffff, shading: THREE.FlatShading } ), // front
+        new THREE.MeshPhongMaterial( { color: 0xffffff, shading: THREE.SmoothShading } ) // side
+    ] );
+    var textGeo = new THREE.TextGeometry( count, {
+
+        font: font,
+
+        size: 70,
+        height: 20,
+        curveSegments: 4,
+        bevelThickness: 2,
+        bevelSize: 1.5,
+        bevelEnabled: true,
+        material: 0,
+        extrudeMaterial: 1
     });
-    var tmaterial = new THREE.MeshBasicMaterial({color:0xffffff});        //物体加上材质
-    var tmesh = new THREE.Mesh(text,tmaterial);
-    tmesh.position.set(-10,-10,-200);
+    textGeo.computeBoundingBox();
+    textGeo.computeVertexNormals();
+    var centerOffset = -0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
+
+    var textMesh = new THREE.Mesh( textGeo, textMaterial );
+    textMesh.position.x = centerOffset;
+    textMesh.position.y = 0;
+    textMesh.position.z = -350;
+    textMesh.rotation.x = 0;
+    textMesh.rotation.y = Math.PI * 2;
+
 
     scene.add(curtain);
-    scene.add(tmesh);
+    scene.add(textMesh);
 
     var count_do = function() {
         if(count <= 0) {
             scene.remove(curtain);
-            scene.remove(tmesh);
+            scene.remove(textMesh);
             callback();
         } else {
             count--;
-            scene.remove(tmesh);
-            text = new THREE.TextGeometry(count,{
-                size: 20,
-                height: 0,
-                weight: 'normal',
-                font:  font,
-                style: 'normal',
-                bevelThickness: 1,
-                bevelSize: 1,
-                bevelSegments: 1,
-                curveSegments: 50,
-                steps: 1
+            scene.remove(textMesh);
+            textGeo = new THREE.TextGeometry( count, {
+
+                font: font,
+
+                size: 70,
+                height: 20,
+                curveSegments: 4,
+                bevelThickness: 2,
+                bevelSize: 1.5,
+                bevelEnabled: true,
+                material: 0,
+                extrudeMaterial: 1
             });
-            tmaterial = new THREE.MeshBasicMaterial({color:0xffffff});        //物体加上材质
-            tmesh = new THREE.Mesh(text,tmaterial);
-            tmesh.position.set(-10,-10,-200);
+            textGeo.computeBoundingBox();
+            textGeo.computeVertexNormals();
+            centerOffset = -0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
+
+            textMesh = new THREE.Mesh( textGeo, textMaterial );
+            textMesh.position.x = centerOffset;
+            textMesh.position.y = 0;
+            textMesh.position.z = -350;
+            textMesh.rotation.x = 0;
+            textMesh.rotation.y = Math.PI * 2;
 
             scene.add(curtain);
-            scene.add(tmesh);
+            scene.add(textMesh);
             setTimeout(count_do,1000);
         }
     };
--- static/js/vr_hc.js
+++ static/js/vr_hc.js
@@ -1,23 +1,52 @@
 function HControlBegin(callback) {
     hc.open();
     //渲染对话框
-    var text = new THREE.TextGeometry('text',{
-        size: 20,
-        height: 0,
-        weight: 'normal',
-        font:  font,
-        style: 'normal',
-        bevelThickness: 1,
-        bevelSize: 1,
-        bevelSegments: 1,
-        curveSegments: 50,
-        steps: 1
-    });
-    var tmaterial = new THREE.MeshBasicMaterial({color:0xffffff});        //物体加上材质
-    var tmesh = new THREE.Mesh(text,tmaterial);
-    tmesh.position.set(-20,-20,-200);
+    var pointLight = new THREE.PointLight( 0xffffff, 1.5 );
+    pointLight.position.set( 0, 100, 90 );
+    scene.add( pointLight );
 
-    scene.add(tmesh);
+    var colorhash  = "00ffff";
+    hex = colorhash;
+    pointLight.color.setHex( parseInt( colorhash, 16 ) );
+
+
+    var curtainGeometry = new THREE.PlaneGeometry( 300, 200 );
+    var curtain = new THREE.Mesh( curtainGeometry ,new THREE.MeshBasicMaterial({
+        color: 0x000000
+    }));
+    curtain.position.set(0,50,-399);
+
+    var textMaterial = new THREE.MultiMaterial( [
+        new THREE.MeshPhongMaterial( { color: 0xffffff, shading: THREE.FlatShading } ), // front
+        new THREE.MeshPhongMaterial( { color: 0xffffff, shading: THREE.SmoothShading } ) // side
+    ] );
+    var textGeo = new THREE.TextGeometry( 'nihao', {
+
+        font: font,
+
+        size: 70,
+        height: 20,
+        curveSegments: 4,
+        bevelThickness: 2,
+        bevelSize: 1.5,
+        bevelEnabled: true,
+        material: 0,
+        extrudeMaterial: 1
+    });
+    textGeo.computeBoundingBox();
+    textGeo.computeVertexNormals();
+    var centerOffset = -0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
+
+    var textMesh = new THREE.Mesh( textGeo, textMaterial );
+    textMesh.position.x = centerOffset;
+    textMesh.position.y = 0;
+    textMesh.position.z = -350;
+    textMesh.rotation.x = 0;
+    textMesh.rotation.y = Math.PI * 2;
+
+
+    scene.add(curtain);
+    scene.add(textMesh);
 
     setTimeout(function(){
         console.log('nod start');
